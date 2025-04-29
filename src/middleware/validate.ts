30a6786086219a6388c5b913b2c054ea5ed03603
Izunnaya <ugorjimicheal.um@gmail.com>
2025-04-29 22:42:45 +0100

    Add MyUser validation middleware and integrate with updateMyUser endpoint

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,10 +1,22 @@
-//This is the file where fields from a request will validated, to check if they correct and of the expected typez
+//This is the file where fields from a request will validated, to check if they correct and of the expected types
 
-import { body } from "express-validator"
+import { NextFunction, Response, Request} from "express"
+import { body, validationResult } from "express-validator"
+
+const handleValidationErrors = async (req:Request, res:Response, next:NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+        res.sendStatus(400).json({ errors: errors.array() });
+        return;
+    }
+    next()
+}
 
 export const validateMyUserRequest = [
     body("name").isString().notEmpty().withMessage("Name Must be a String"),
-    body("addressLine1").isString().isEmpty().withMessage("Address must be a String"),
-    body("city").isString().isEmpty().withMessage("City must be a String"),
-    body("country").isString().isEmpty().withMessage("Country must be a String")
+    body("addressLine1").isString().notEmpty().withMessage("Address must be a String"),
+    body("city").isString().notEmpty().withMessage("City must be a String"),
+    body("country").isString().notEmpty().withMessage("Country must be a String"),
+    handleValidationErrors,
 ]
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,3 +1,4 @@
+import { validateMyUserRequest } from './../middleware/validate';
 import { jwtCheck, jwtParse } from './../middleware/auth';
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
@@ -7,6 +8,6 @@ const router = express.Router();
 
 // Once the request coming from /api/my/user is a post request, it get's forwarded to "MyUserController.createCurrentUser"
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
 
 export default router;
